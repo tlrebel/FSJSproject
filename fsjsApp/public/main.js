c3974b1831707ebe6cc3f36b64dcd190ff24c8c0
Tricia <robetric@isu.edu>
2018-03-26 11:02:12 -0400

    work in  progress.

--- fsjsApp/public/main.js
+++ fsjsApp/public/main.js
@@ -1,17 +1,38 @@
 function start(){
+    clocking()
+            
+        // To check the input from the user 
+        var numbersUser = /^[0-9]+$/;
+        var userInput = document.getElementById("numberID").value;
     
-            var numbersUser = /^[0-9]+$/;
-          
-            var userInput = document.getElementById("numberID").value;
-            console.log(numbersUser.value);
+            // Validation that the length and input of being numbers only are correct.
             if (isNaN(userInput) || userInput.length < 7) {
                 alert("Please type only 7 numbers.");
             } else {
-                alert("You have clocked in.");
+                
+        // Setting the date of the time the button is clicked.
+        var currentDate = new Date();
+        var thisDay = (currentDate.getMonth()+ 1) + '-' + currentDate.getDate() + '-' + currentDate.getFullYear();
+        var currentTime = currentDate.getHours() + ':' + currentDate.getMinutes() + ':' + currentDate.getSeconds();
+        var punchTime = thisDay + ' ' + currentTime;
+                
+            //
+             const ajaxSettings = {
+                data: {number: userInput},
+                method: 'POST',
+                success: function (data, textStatus, jqXHR) {
+                    console.log(data, textStatus, jqXHR)
+                   $("#timestamps").html(data)
+                }
             }
-        
+            $.ajax("/users/timestamps", ajaxSettings)      
+        }       
 }
 
 function end(){
-  
+  clocking()
+}
+
+function clocking() {
+    
 }
--- fsjsApp/routes/users.js
+++ fsjsApp/routes/users.js
@@ -1,29 +1,43 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
 
-///* GET users listing. */
-//router.get('/', function(req, res, next) {
-//  database.thing = database.thing + 1
-//  res.send(database);
-//});
+mongoose.connect('mongodb://admin:password@ds125255.mlab.com:25255/timesheet');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('we\'re connected!')
+});
+
+var kittySchema = mongoose.Schema({
+  name: String
+});
+var Kitten = mongoose.model('Kitten', kittySchema);
+
 
 var database = {
     thing: 1
 }
-
+// GET
 router.get('/timestamps', function(req, res, next) {
-  res.send('whatever')
+  console.log(req.query)
+    res.send('whatever')
+});
+//create 
+router.post('/timestamps', function(req, res, next){
+   console.log(req.body)
+    var timeSchema = mongoose.Schema({
+        date: Date, userId:Number
+    });
+    var Timestamp = mongoose.model('Timestamp', timeSchema);
+    var timestamp = new Timestamp({ userId: 5764356 });
+    console.log(timestamp.userId); 
+    timestamp.save(function (err, timestamp) {
+        if (err) return console.error(err);
+      });
+    res.send('do not cry')
 });
 
-
-router.get('/thing', function(req, res, next) {
-  database.thing = database.thing + 1
-  res.send(database);
-});
-
-router.get('/thing3', function(req, res, next) {
-  database.thing = database.thing + 3
-  res.send(database);
-});
+router.get('/')
 
 module.exports = router;
