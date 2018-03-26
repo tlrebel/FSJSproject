107c0bf0ce22fc303b9b4cec80f7f6b3b9e8a619
Tricia <robetric@isu.edu>
2018-03-26 17:26:02 -0400

    attempting to fix server error.

--- fsjsApp/app.js
+++ fsjsApp/app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-//var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
@@ -22,7 +21,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
@@ -30,6 +28,12 @@ app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
+});
+
+// Connection
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('we\'re connected!')
 });
 
 // error handler
@@ -45,8 +49,4 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-// Connection
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('we\'re connected!')
-});
+
